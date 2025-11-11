80654f5ab6ea807f2cfbd4c0775d2fdae5d78bb2
Federico Ruiz Ugalde <memeruiz@gmail.com>
2025-11-11 11:17:13 -0600

    stm32f429discovery draw examples

---
+++ examples/stm32/f4/stm32f429idiscovery/lcd_touch_cdcacm/planets.c
@@ -0,0 +1,106 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2014 Chuck McManis <cmcmanis@mcmanis.com>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <math.h>
+#include <stdint.h>
+
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/systick.h>
+#include <libopencm3/stm32/rcc.h>
+
+#include <libopencm3-plus/utils/misc.h>
+
+#include <libopencm3-plus/hw-accesories/cm3/clock.h>
+#include <libopencm3-plus/hw-accesories/tft_lcd/i2c-lcd-touch.h>
+#include <libopencm3-plus/hw-accesories/tft_lcd/lcd-serial-touch.h>
+#include <libopencm3-plus/hw-accesories/lcd/lcd-spi.h>
+#include <libopencm3-plus/hw-accesories/sdram_stm32f429idiscovery.h>
+
+#define SLEEP_TIME 2000
+
+void clock_setup(void) {
+  const uint32_t one_milisecond_rate = 168000;
+  /* Base board frequency, set to 168Mhz */
+  rcc_clock_setup_pll(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
+
+  /* clock rate / 168000 to get 1mS interrupt rate */
+  systick_set_reload(one_milisecond_rate);
+  systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
+  systick_counter_enable();
+
+  /* this done last */
+  systick_interrupt_enable();
+}
+
+/*
+ * This is our example, the heavy lifing is actually in lcd-spi.c but
+ * this drives that code.
+ */
+int main(void) {
+  int planet_1, planet_2, planet_3;
+
+  clock_setup();
+  sdram_init();
+  lcd_spi_init();
+  msleep(SLEEP_TIME);
+  gfx_init(lcd_draw_pixel, LCD_WIDTH, LCD_HEIGHT);
+  gfx_fillScreen(LCD_GREY);
+  gfx_fillRoundRect(10, 10, 220, 220, 5, LCD_WHITE);
+  gfx_drawRoundRect(10, 10, 220, 220, 5, LCD_RED);
+  gfx_fillCircle(20, 250, 10, LCD_RED);
+  gfx_fillCircle(120, 250, 10, LCD_GREEN);
+  gfx_fillCircle(220, 250, 10, LCD_BLUE);
+  gfx_setTextSize(2);
+  gfx_setCursor(15, 25);
+  gfx_puts("STM32F4-DISCO");
+  gfx_setTextSize(1);
+  gfx_setCursor(15, 49);
+  gfx_puts("Simple example to put some");
+  gfx_setCursor(15, 60);
+  gfx_puts("stuff on the LCD screen.");
+  lcd_show_frame();
+  msleep(SLEEP_TIME);
+  gfx_setTextColor(LCD_YELLOW, LCD_BLACK);
+  gfx_setTextSize(3);
+  planet_1 = 0;
+  planet_2 = 45;
+  planet_3 = 90;
+  while (1) {
+    gfx_fillScreen(LCD_BLACK);
+    gfx_setCursor(15, 36);
+    gfx_puts("PLANETS!");
+    gfx_fillCircle(120, 160, 40, LCD_YELLOW);
+    gfx_drawCircle(120, 160, 55, LCD_GREY);
+    gfx_drawCircle(120, 160, 75, LCD_GREY);
+    gfx_drawCircle(120, 160, 100, LCD_GREY);
+
+    gfx_fillCircle(120 + (sin(degrees_to_radians(planet_1)) * 55),
+                   160 + (cos(degrees_to_radians(planet_1)) * 55), 5, LCD_RED);
+    gfx_fillCircle(120 + (sin(degrees_to_radians(planet_2)) * 75),
+                   160 + (cos(degrees_to_radians(planet_2)) * 75), 10,
+                   LCD_WHITE);
+    gfx_fillCircle(120 + (sin(degrees_to_radians(planet_3)) * 100),
+                   160 + (cos(degrees_to_radians(planet_3)) * 100), 8,
+                   LCD_BLUE);
+    planet_1 = (planet_1 + 3) % 360;
+    planet_2 = (planet_2 + 2) % 360;
+    planet_3 = (planet_3 + 1) % 360;
+    lcd_show_frame();
+  }
+}
---
+++ examples/stm32/f4/stm32f429idiscovery/lcd_touch_cdcacm/print_touched_coord.c
@@ -0,0 +1,102 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2014 Chuck McManis <cmcmanis@mcmanis.com>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include <libopencm3/cm3/systick.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/i2c.h>
+#include <libopencm3/stm32/rcc.h>
+
+#include <libopencm3-plus/hw-accesories/cm3/clock.h>
+#include <libopencm3-plus/hw-accesories/tft_lcd/i2c-lcd-touch.h>
+#include <libopencm3-plus/hw-accesories/tft_lcd/lcd-serial-touch.h>
+#include <libopencm3-plus/hw-accesories/lcd/lcd-spi.h>
+#include <libopencm3-plus/hw-accesories/sdram_stm32f429idiscovery.h>
+
+void tft_init(void);
+
+void clock_setup(void) {
+  const uint32_t one_milisecond_rate = 168000;
+  /* Base board frequency, set to 168Mhz */
+  rcc_clock_setup_pll(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
+
+  /* clock rate / 168000 to get 1mS interrupt rate */
+  systick_set_reload(one_milisecond_rate);
+  systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
+  systick_counter_enable();
+
+  /* this done last */
+  systick_interrupt_enable();
+}
+
+void tft_init(void) {
+  i2c_init();
+
+  tft_enable_clocks(false);
+  tft_touchscreen_enable_controller_control(false);
+
+  tft_touchscreen_configure_controller_control(TSC_XYZ_AXIS, TRACK_8);
+  tft_touchscreen_apply_controller_configuration(MICRO_S_500_SET, MICRO_S_500_D,
+                                                 SAMPLE_2);
+  tft_set_fifo_level_interrupt(0x1);
+
+  tft_touchscreen_enable_controller_control(true);
+  tft_enable_clocks(true);
+}
+
+int main(void) {
+  clock_setup();
+  sdram_init();
+  lcd_spi_init();
+  tft_init();
+
+  gfx_init(lcd_draw_pixel, 240, 320);
+  gfx_setTextColor(LCD_YELLOW, LCD_BLACK);
+  gfx_setTextSize(LCD_TEXT_MIN_SIZE);
+  lcd_show_frame();
+
+  int x = 1;
+  int y = 1;
+  uint8_t current_fifo_size = 0;
+  uint16_t datax = 0;
+  uint16_t datay = 0;
+  uint16_t dataz = 0;
+  char sData[64];
+
+  while (true) {
+    gfx_fillScreen(LCD_BLACK);
+    gfx_setCursor((int)(LCD_WIDTH / 4), (int)(LCD_HEIGHT / 2));
+
+    current_fifo_size = tft_get_fifo_size();
+
+    while (current_fifo_size > 0) {
+      tft_get_coord_data_access(X_COORD, &datax);
+      tft_get_coord_data_access(Y_COORD, &datay);
+      tft_get_coord_data_access(Z_COORD, &dataz);
+      current_fifo_size = tft_get_fifo_size();
+    }
+    tft_convert_touch_coord_to_lcd_coord(datax, datay, &x, &y);
+    sprintf(sData, "%u %u", x, y);
+    gfx_puts(sData);
+    lcd_show_frame();
+  }
+}
